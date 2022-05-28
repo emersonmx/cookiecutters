a322b32708cb44eba0352f299f37e1028d06bf74
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-05-28 03:00:27 -0300

    [python] Add invoke tests template

---
+++ python/invoke/tests/{{cookiecutter.path}}/_tasks_snippet.py
@@ -0,0 +1,27 @@
+from invoke import Context, task
+
+
+@task
+def tests(c, quiet=False):
+    # type: (Context, bool) -> None
+    pytest_options: list[str] = []
+
+    if quiet:
+        pytest_options.append("-q")
+
+    cmd = " ".join(
+        [
+            "coverage",
+            "run",
+            "-m",
+            "pytest",
+            *pytest_options,
+        ],
+    )
+    c.run(cmd, pty=True)
+
+
+@task
+def coverage(c):
+    # type: (Context) -> None
+    c.run("coverage report", pty=True)
