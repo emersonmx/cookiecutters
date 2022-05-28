373b34263dafbc762897a53e63bc957743b9ab33
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-05-28 02:55:55 -0300

    [python] Add invoke lint template

---
+++ python/invoke/lint/{{cookiecutter.path}}/_tasks_snippet.py
@@ -0,0 +1,34 @@
+from invoke import Context, task
+
+
+@task
+{%- if cookiecutter.use_pre_commit == "true" %}
+def lint(c, all_files=False):
+    # type: (Context, bool) -> None
+    precommit_options = []
+
+    if all_files:
+        precommit_options.append("--all-files")
+
+    hooks = [
+        "check-added-large-files",
+        "check-merge-conflict",
+        "debug-statements",
+        "detect-private-key",
+        "name-tests-test",
+        "flake8",
+        "mypy",
+        "vulture",
+        "bandit",
+    ]
+    for hook in hooks:
+        cmd = " ".join(["pre-commit", "run", *precommit_options, hook])
+        c.run(cmd, pty=True)
+    {%- else %}
+def lint(c):
+    # type: (Context) -> None
+    c.run("flake8 .")
+    c.run("mypy --strict .")
+    c.run("vulture .")
+    c.run("bandit -r .")
+    {%- endif %}
