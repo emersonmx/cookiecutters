ed65473edac92a0f9426ea73e04544528e58e7db
Emerson Max de Medeiros Silva <emersonmx@gmail.com>
2022-11-24 18:24:53 -0300

    Remove project templates

--- tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-from invoke import Collection
-
-from tasks import projects
-
-ns = Collection()
-ns.add_collection(projects)
--- tasks/projects/__init__.py
+++
@@ -1,6 +0,0 @@
-from invoke import Collection
-
-from tasks.projects import python
-
-ns = Collection()
-ns.add_collection(python)
